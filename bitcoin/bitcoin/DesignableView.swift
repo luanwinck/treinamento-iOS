286e51d846049a1868294ecd473a2afda93c71b5
luan winck <luanwinck@cwi.com.br>
2019-06-01 15:47:23 -0300

    feat(bitcoin): created first screen

---
+++ bitcoin/bitcoin/DesignableView.swift
@@ -0,0 +1,12 @@
+//
+//  DesignableView.swift
+//  bitcoin
+//
+//  Created by Luan Winck on 01/06/19.
+//  Copyright © 2019 CWI software. All rights reserved.
+//
+
+import UIKit
+
+@IBDesignable
+class DesignableView: UIView {}
---
+++ bitcoin/bitcoin/UIViewExtensions.swift
@@ -0,0 +1,39 @@
+//
+//  UIViewExtensions.swift
+//  bitcoin
+//
+//  Created by Luan Winck on 01/06/19.
+//  Copyright © 2019 CWI software. All rights reserved.
+//
+
+import UIKit
+
+extension UIView {
+    @IBInspectable var cornerRadius: CGFloat {
+        get {
+            return layer.cornerRadius
+        }
+        set {
+            layer.cornerRadius = newValue
+            layer.masksToBounds = newValue > 0
+        }
+    }
+    
+    @IBInspectable var borderWidth: CGFloat {
+        get {
+            return layer.borderWidth
+        }
+        set {
+            layer.borderWidth = newValue
+        }
+    }
+    
+    @IBInspectable var borderColor: UIColor? {
+        get {
+            return UIColor(cgColor: layer.borderColor!)
+        }
+        set {
+            layer.borderColor = newValue?.cgColor
+        }
+    }
+}
