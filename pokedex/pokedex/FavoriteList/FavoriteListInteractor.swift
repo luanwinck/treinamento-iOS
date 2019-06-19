be37379260a95efea61bd4ef6b8d7774fcedab7a
Luan Winck <luan.winck@cwi.com.br>
2019-06-19 09:59:04 -0300

    created favorist list

---
+++ pokedex/pokedex/FavoriteList/FavoriteListInteractor.swift
@@ -0,0 +1,9 @@
+//
+//  FavoriteListInteractor.swift
+//  Pokedex
+//
+//  Created by Luan Winck on 19/06/19.
+//  Copyright © 2019 CWI Software. All rights reserved.
+//
+
+import Foundation
---
+++ pokedex/pokedex/FavoriteList/FavoritelListController.swift
@@ -0,0 +1,9 @@
+//
+//  FavoritelListController.swift
+//  Pokedex
+//
+//  Created by Luan Winck on 19/06/19.
+//  Copyright © 2019 CWI Software. All rights reserved.
+//
+
+import Foundation
